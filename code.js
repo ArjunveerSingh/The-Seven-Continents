5129de1e40f890335ccd5cca4558664edfd4d139
ArjunveerSingh <76784986+ArjunveerSingh@users.noreply.github.com>
2022-01-02 02:00:45 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,235 @@
+setTimeout(function() {
+  setScreen("screen2");
+  new Audio ('https://freetts.com/audio/34455c2b-568a-47c8-b633-9e202a21dcae.mp3').play()  
+}, 1000);
+onEvent("button1", "click", function( ) {
+  setScreen("screen3");
+});
+onEvent("button8", "click", function( ) {
+  setScreen("screen4");
+});
+onEvent("button10", "click", function( ) {
+  setScreen("screen5");
+ new Audio('https://s17.aconvert.com/convert/p3r68-cdx67/ksp5g-mgs8z.mp3').play()
+});
+onEvent("button9", "click", function( ) {
+  setScreen("screen3");
+});
+onEvent("button11", "click", function( ) {
+  open("https://www.countryaah.com/asian-countries/");
+});
+onEvent("label16", "click", function( ) {
+  open("https://www.countryaah.com/asian-countries/");
+});
+onEvent("button12", "click", function( ) {
+  open("https://www.nationalgeographic.org/encyclopedia/asia/");
+});
+onEvent("label17", "click", function( ) {
+  open("https://www.nationalgeographic.org/encyclopedia/asia/");
+});
+onEvent("button2", "click", function( ) {
+  setScreen("screen7");
+});
+onEvent("button13", "click", function( ) {
+  setScreen("screen9");
+});
+onEvent("button14", "click", function( ) {
+  setScreen("screen7");
+});
+onEvent("button15", "click", function( ) {
+  setScreen("screen11");
+  new Audio ('https://freetts.com/audio/af33528b-0c2a-4eca-bf05-33c4ef6c2c48.mp3').play()
+});
+onEvent("button16", "click", function( ) {
+  open("https://www.countryaah.com/african-countries/");
+});
+onEvent("label27", "click", function( ) {
+  open("https://www.countryaah.com/african-countries/");
+});
+onEvent("button17", "click", function( ) {
+  open("https://www.nationalgeographic.org/encyclopedia/africa-physical-geography/");
+});
+onEvent("label28", "click", function( ) {
+  open("https://www.nationalgeographic.org/encyclopedia/africa-physical-geography/");
+});
+onEvent("button3", "click", function( ) {
+  setScreen("screen12");
+});
+onEvent("button18", "click", function( ) {
+  setScreen("screen6");
+});
+onEvent("button19", "click", function( ) {
+  setScreen("screen12");
+});
+onEvent("button20", "click", function( ) {
+  setScreen("screen8");
+  new Audio ('https://freetts.com/audio/fa2c79c8-a020-4ed0-97d8-810e58e65d3c.mp3')
+});
+onEvent("button21", "click", function( ) {
+  open("https://www.countryaah.com/north-american-countries/");
+});
+onEvent("label35", "click", function( ) {
+  open("https://www.countryaah.com/north-american-countries/");
+});
+onEvent("button22", "click", function( ) {
+  open("https://www.nationalgeographic.org/encyclopedia/north-america-physical-geography/");
+});
+onEvent("label36", "click", function( ) {
+  open("https://www.nationalgeographic.org/encyclopedia/north-america-physical-geography/");
+});
+onEvent("button23", "click", function( ) {
+  setScreen("screen13");
+});
+onEvent("button25", "click", function( ) {
+  setScreen("screen14");
+  new Audio ('https://freetts.com/audio/701fc7d6-a5f4-4b5d-8420-7668381a5d73.mp3').play()
+});
+onEvent("button24", "click", function( ) {
+  setScreen("screen10");
+});
+onEvent("button26", "click", function( ) {
+  open("https://www.countryaah.com/south-american-countries/");
+});
+onEvent("label43", "click", function( ) {
+  open("https://www.countryaah.com/south-american-countries/");
+});
+onEvent("button27", "click", function( ) {
+  open("https://www.nationalgeographic.org/encyclopedia/south-america-physical-geography/");
+});
+onEvent("label44", "click", function( ) {
+  open("https://www.nationalgeographic.org/encyclopedia/south-america-physical-geography/");
+});
+onEvent("button6", "click", function( ) {
+  setScreen("screen15");
+});
+onEvent("button28", "click", function( ) {
+  setScreen("screen16");
+});
+onEvent("button30", "click", function( ) {
+  setScreen("screen17");
+});
+onEvent("button29", "click", function( ) {
+  setScreen("screen15");
+});
+onEvent("button32", "click", function( ) {
+  setScreen("screen18");
+  new Audio ('https://freetts.com/audio/b6d98399-46e2-4edd-8c36-74674e2797b0.mp3').play()
+});
+onEvent("button33", "click", function( ) {
+  open("https://www.isolatedtraveller.com/world-encyclopedia/list-of-countries-in-antarctica/");
+});
+onEvent("label56", "click", function( ) {
+  open("https://www.isolatedtraveller.com/world-encyclopedia/list-of-countries-in-antarctica/");
+});
+onEvent("button34", "click", function( ) {
+  open("https://www.nationalgeographic.org/encyclopedia/antarctica/");
+});
+onEvent("label57", "click", function( ) {
+  open("https://www.nationalgeographic.org/encyclopedia/antarctica/");
+});
+onEvent("button5", "click", function( ) {
+  setScreen("screen19");
+});
+onEvent("button35", "click", function( ) {
+  setScreen("screen20");
+});
+onEvent("button37", "click", function( ) {
+  setScreen("screen21");
+  new Audio ('https://freetts.com/audio/14e810f7-17f6-489e-9d67-8ea0c70d27ff.mp3').play()
+});
+onEvent("button36", "click", function( ) {
+  setScreen("screen19");
+});
+onEvent("button38", "click", function( ) {
+  open("https://www.countryaah.com/european-countries/");
+});
+onEvent("label63", "click", function( ) {
+  open("https://www.countryaah.com/european-countries/");
+});
+onEvent("button39", "click", function( ) {
+  open("https://www.nationalgeographic.org/encyclopedia/europe-human-geography/");
+});
+onEvent("label64", "click", function( ) {
+  open("https://www.nationalgeographic.org/encyclopedia/europe-human-geography/");
+});
+onEvent("button7", "click", function( ) {
+  setScreen("screen22");
+});
+onEvent("button40", "click", function( ) {
+  setScreen("screen23");
+});
+onEvent("button42", "click", function( ) {
+  setScreen("screen24");
+  new Audio ('https://freetts.com/audio/739079da-d7bf-4dfc-a69e-94f761f7e409.mp3').play()
+});
+onEvent("button41", "click", function( ) {
+  setScreen("screen22");
+});
+onEvent("button43", "click", function( ) {
+  open("https://www.countryaah.com/oceania-countries/");
+});
+onEvent("label72", "click", function( ) {
+  open("https://www.countryaah.com/oceania-countries/");
+});
+onEvent("button44", "click", function( ) {
+  open("https://www.nationalgeographic.org/encyclopedia/oceania-physical-geography/");
+});
+onEvent("label73", "click", function( ) {
+  open("https://www.nationalgeographic.org/encyclopedia/oceania-physical-geography/");
+});
+onEvent("button45", "click", function( ) {
+  setScreen("screen2");
+  new Audio ('https://freetts.com/audio/34455c2b-568a-47c8-b633-9e202a21dcae.mp3').play()
+});
+onEvent("button46", "click", function( ) {
+  setScreen("screen4");
+});
+onEvent("button47", "click", function( ) {
+  setScreen("screen2");
+  new Audio ('https://freetts.com/audio/34455c2b-568a-47c8-b633-9e202a21dcae.mp3').play()
+});
+onEvent("button48", "click", function( ) {
+  setScreen("screen9");
+});
+onEvent("button49", "click", function( ) {
+  setScreen("screen2");
+  new Audio ('https://freetts.com/audio/34455c2b-568a-47c8-b633-9e202a21dcae.mp3').play()
+});
+onEvent("button50", "click", function( ) {
+  setScreen("screen2");
+  new Audio ('https://freetts.com/audio/34455c2b-568a-47c8-b633-9e202a21dcae.mp3').play()
+});
+onEvent("button51", "click", function( ) {
+  setScreen("screen13");
+});
+onEvent("button52", "click", function( ) {
+  setScreen("screen2");
+  new Audio ('https://freetts.com/audio/34455c2b-568a-47c8-b633-9e202a21dcae.mp3').play()
+});
+onEvent("button53", "click", function( ) {
+  setScreen("screen17");
+});
+onEvent("button54", "click", function( ) {
+  setScreen("screen20");
+});
+onEvent("button55", "click", function( ) {
+  setScreen("screen2");
+  new Audio ('https://freetts.com/audio/34455c2b-568a-47c8-b633-9e202a21dcae.mp3').play()
+});
+onEvent("button56", "click", function( ) {
+  setScreen("screen23");
+});
+onEvent("button4", "click", function( ) {
+  setScreen("screen10");
+});
+onEvent("button57", "click", function( ) {
+  setScreen("screen2");
+  new Audio ('https://freetts.com/audio/34455c2b-568a-47c8-b633-9e202a21dcae.mp3').play()
+});
+onEvent("button58", "click", function( ) {
+  setScreen("screen6");
+});
+onEvent("button31", "click", function( ) {
+  setScreen("screen16");
+});
+
